0cb72052cca0b1002ef197bcc54a839f68c3cf5f
jbhanks <theia@theiadocker-jhanks1981>
2024-11-26 14:23:03 -0500

    Up to exercise 2 with pasting the given code, test coverage still not what it is supposed to be

--- tests/test_models.py
+++ tests/test_models.py
@@ -116,7 +116,7 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found_product.name, product.name)
         self.assertEqual(found_product.description, product.description)
         self.assertEqual(found_product.price, product.price)
-    
+
     def test_update_a_product(self):
         """It should Update a Product"""
         product = ProductFactory()
@@ -157,7 +157,7 @@ class TestProductModel(unittest.TestCase):
         products = Product.all()
         self.assertEqual(len(products), 5)
 
-    def test_find_by_name(self):
+     def test_find_by_name(self):
         """It should Find a Product by Name"""
         products = ProductFactory.create_batch(5)
         for product in products:
@@ -168,7 +168,6 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.name, name)
-
 
     def test_find_by_availability(self):
         """It should Find Products by Availability"""
